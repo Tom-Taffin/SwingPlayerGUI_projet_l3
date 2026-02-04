75c6f22f74d2a3865bb768b5f26e2e366c608984
Samuel DEMON <samuel.demon.etu@univ-lille.fr>
2026-02-04 09:28:47 +0100

    feat: image rotation support

--- src/main/java/l3s6/projet/star/gui/BoardPanel.java
+++ src/main/java/l3s6/projet/star/gui/BoardPanel.java
@@ -6,6 +6,8 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import l3s6.projet.star.game.board.Board;
+import l3s6.projet.star.game.tile.*;
 
 public class BoardPanel extends JPanel {
 
@@ -13,7 +15,7 @@ public class BoardPanel extends JPanel {
       this.setLayout(new GridLayout(5, 5));
       this.setBackground(Color.LIGHT_GRAY);
 
-      List<String> givenList = Arrays.asList("f1r1f2-f2r2f3-f3r3f4-f4r4f1", "c-c-f-f", "c1-f-f-c2", "c-f1rf2-f2-f2rf1", "C-C-C-C", "empty");
+      List<String> givenList = Arrays.asList("Wf1r1f2-f2r2f3-f3r3f4-f4r4f1", "Ec-c-f-f", "Nc1-f-f-c2", "Sc-f1rf2-f2-f2rf1", "SC-C-C-C", "empty");
       Random rand = new Random();
       for (int i = 0; i < 25; i++){
          String randomTileName = givenList.get(rand.nextInt(givenList.size()));
--- src/main/java/l3s6/projet/star/gui/TilePanel.java
+++ src/main/java/l3s6/projet/star/gui/TilePanel.java
@@ -2,24 +2,61 @@ package l3s6.projet.star.gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.util.Map;
 
 public class TilePanel extends JPanel {
+   private Character rotation;
    private String tileName;
 
    public TilePanel(String tileName) {
-      this.tileName = tileName;
-      this.setBackground(Color.WHITE);
+      if (tileName.equals("empty")){
+         this.rotation = null;
+         this.tileName = tileName;
+      }
+      else if ("NWES".contains(tileName.substring(0, 1))){
+         this.rotation = tileName.charAt(0);
+         this.tileName = tileName.substring(1);
+      }
+      else {
+         this.rotation = 'N';
+         this.tileName = tileName;
+      }
+
+         this.setBackground(Color.WHITE);
    }
+
+   private static final Map<Character, Double> ROTATIONS = Map.of(
+           'N', 0.0,
+           'E', Math.PI / 2,
+           'S', Math.PI,
+           'W', -Math.PI / 2
+   );
 
    @Override
    protected void paintComponent(Graphics g) {
       super.paintComponent(g);
-      
+
       Image img = TileManager.getInstance().getImage(this.tileName);
-      
-      if (img != null) {
-         g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
+      if (img == null) return;
+
+      Graphics2D g2 = (Graphics2D) g;
+      g2.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
+              RenderingHints.VALUE_INTERPOLATION_BILINEAR);
+
+      int w = getWidth();
+      int h = getHeight();
+
+      AffineTransform oldTransform = g2.getTransform();
+
+      if (rotation != null) {
+         double angle = ROTATIONS.getOrDefault(rotation, 0.0);
+         g2.rotate(angle, w / 2.0, h / 2.0);
       }
+
+      g2.drawImage(img, 0, 0, w, h, this);
+
+      g2.setTransform(oldTransform);
    }
 
 }

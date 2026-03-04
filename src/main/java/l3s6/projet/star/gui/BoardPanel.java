a89ebb4b4a79626eb05b6d205fdd6469b040cccb
Rémi Andrieu <remi.andrieu@outlook.fr>
2026-03-04 19:38:27 +0100

    feat: automatic sizing for the board

--- src/main/java/l3s6/projet/star/gui/BoardPanel.java
+++ src/main/java/l3s6/projet/star/gui/BoardPanel.java
@@ -3,22 +3,55 @@ package l3s6.projet.star.gui;
 import javax.swing.*;
 import java.awt.*;
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import l3s6.projet.star.game.board.Board;
+import l3s6.projet.star.game.board.Coordinates;
+import l3s6.projet.star.game.tile.TileBuilder;
+import l3s6.projet.star.game.tile.WrongTileSyntaxException;
 
 public class BoardPanel extends JPanel {
 
-   public BoardPanel() throws IOException, ImageNotFoundException {
-      this.setLayout(new GridLayout(5, 5));
-      this.setBackground(Color.LIGHT_GRAY);
+   protected Board board;
 
-      List<String> givenList = Arrays.asList("Wc1-f2r3f4-f4r5f6-f6r7f2", "Ef1-C2-f3-C2", "Nf1-f1-f1-f1:A", "Sf1r2f3-f3r4f5-f5r6f7-f7r8f1", "SC1-C1-f2r3f4-C1", "Nempty");
-      Random rand = new Random();
-      for (int i = 0; i < 25; i++){
-         String randomTileName = givenList.get(rand.nextInt(givenList.size()));
-         this.add(new TilePanel(randomTileName));
+   public BoardPanel() throws IOException, ImageNotFoundException {
+      this.board = new Board();
+      this.setBackground(Color.BLACK);
+      this.createTilePanel();
+   }
+
+   public void addTile(String tileName, Coordinates coord) throws WrongTileSyntaxException, ImageNotFoundException{
+      this.board.putTileAt(new TileBuilder().build(tileName), coord);
+      this.createTilePanel();
+   }
+
+   public void createTilePanel() throws ImageNotFoundException{
+      this.removeAll();
+
+      int width = this.board.getMaxWidth() - this.board.getMinWidth() + 1;
+      int height = this.board.getMaxHeight() - this.board.getMinHeight() + 1;
+
+      int gridSize = Math.max(width, height) + 2;
+      gridSize = Math.max(gridSize, 5);
+
+      this.setLayout(new GridLayout(gridSize, gridSize));
+
+      int midX = (this.board.getMaxWidth() + this.board.getMinWidth()) / 2;
+      int midY = (this.board.getMaxHeight() + this.board.getMinHeight()) / 2;
+      
+      int half = gridSize / 2;
+                  
+      for (int i = midY + half; i > midY - (gridSize - half); i--) {
+         for (int j = midX - half; j < midX + (gridSize - half); j++) {
+            Coordinates currentCoord = new Coordinates(j, i);
+            if (this.board.hasTile(currentCoord)) {
+               this.add(new TilePanel(this.board.getTileAt(currentCoord).toString()));
+            } else {
+               this.add(new TilePanel("Nempty")); 
+            }
+         }
       }
+
+      revalidate();
+      repaint();
    }
 
    @Override
@@ -31,7 +64,4 @@ public class BoardPanel extends JPanel {
       return super.getPreferredSize();
    }
 
-   public int getNbOfTilesVertical() {
-      return 5;
-   }
 }
